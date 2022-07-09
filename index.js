65bd6137adaad75816ed4a83c5fdb3c3ed369d07
AlexUAKH <sashaos1404@gmail.com>
2022-07-09 17:42:29 +0300

    websockets added

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
 const wsServer = require("express-ws")(app);
+const aWss = wsServer.getWss();
 
 const PORT = process.env.PORT || 4500;
 
-app.ws("/", (res, req) => {
-  console.log("connected ws");
+app.ws("/ws", (ws, req) => {
+  console.log(req)
+  ws.on('message', msg => {
+    switch (msg.type) {
+      case 'connection':
+        connectionHandler(ws, msg);
+        break;
+    }
+  })
 });
 
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
+
+const connectionHandler = (ws, msg) => {
+  ws.id = msg.id;
+  broadcastConnection(ws, msg);
+}
+
+const broadcastConnection = (ws, msg) => {
+  aWss.clients.forEach(client => {
+    if (client.id === msg.id) {
+      client.send(`User ${msg.username} connected`);
+    }
+  })
+}
