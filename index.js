d98dd5a6ddc4e3f9bc52e0e1794804eac040092e
AlexUAKH <sashaos1404@gmail.com>
2022-07-11 17:43:27 +0300

    corse added

--- index.js
+++ index.js
@@ -2,15 +2,53 @@ const express = require("express");
 const app = express();
 const wsServer = require("express-ws")(app);
 const aWss = wsServer.getWss();
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 4500;
 
-app.ws("/ws", (ws, req) => {
-  console.log(req)
+app.use(cors());
+app.use(express.json());
+
+app.post('/picture/:id', (req, res) => {
+  try {
+    const data = req.body.img.replace("data:image/png;base64,", '');
+    const fileName = req.params.id + '.jpg';
+    const filePath = path.resolve(__dirname, 'static');
+    if (!fs.existsSync(filePath)) {
+      fs.mkdirSync(filePath);
+    }
+    fs.writeFileSync(path.resolve(filePath, fileName), data, "base64");
+    return res.status(200);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json("Cant save picture");
+  }
+})
+app.get('/picture/:id', (req, res) => {
+  try {
+    const fileName = req.params.id + '.jpg';
+    const data = fs.readFileSync(path.resolve(__dirname, "static", fileName), "base64");
+    const picture = `data:image/png;base64,${data}`;
+    return res.status(200).json(picture);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json("Cant find picture");
+  }
+})
+
+app.ws("/ws", (ws) => {
   ws.on('message', msg => {
+    msg = JSON.parse(msg);
     switch (msg.type) {
       case 'connection':
         connectionHandler(ws, msg);
+        break;
+      case 'draw':
+      case 'clear':
+      case 'undo':
+        broadcastConnection(ws, msg);
         break;
     }
   })
@@ -26,7 +64,7 @@ const connectionHandler = (ws, msg) => {
 const broadcastConnection = (ws, msg) => {
   aWss.clients.forEach(client => {
     if (client.id === msg.id) {
-      client.send(`User ${msg.username} connected`);
+      client.send(JSON.stringify(msg));
     }
   })
 }
